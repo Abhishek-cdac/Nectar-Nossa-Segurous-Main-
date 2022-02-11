7c05cabc48b2f9419dae7dca6bb81b46a75b55b7
akashyadavnectar <akash.yadav@nectarinfotel.com>
2022-02-11 16:40:36 +0530

    reimburstment page changed for hr

--- src/Hr/Client/Reimbursment/HrReimbursment.js
+++ src/Hr/Client/Reimbursment/HrReimbursment.js
@@ -118,7 +118,7 @@ const HrReimbursment = () => {
             tableDataArr.push(value);
           });
         setClinicTableData(tableDataArr);
-        setClinicalData(tableDataArr)
+        setClinicalData(tableDataArr);
       } catch (error) {
         console.log("error", error);
         // showAlert('In valide data', "error");
@@ -146,7 +146,7 @@ const HrReimbursment = () => {
             tableDataArr.push(value);
           });
         setPharmacyTableData(tableDataArr);
-        setPharmacyData(tableDataArr)
+        setPharmacyData(tableDataArr);
       } catch (error) {
         console.log("error", error);
         // showAlert('In valide data', "error");
@@ -154,14 +154,12 @@ const HrReimbursment = () => {
     }
   };
 
-
   const handleFilterData = (filterData) => {
     const tableDataArr = [];
     console.log("tr", tableDataArr);
     console.log("filterData", filterData);
     if (filterData.length > 0) {
       if (step === 0) {
-        
         filterData.map((data, i) => {
           const value = {
             SrNo: i,
@@ -196,15 +194,14 @@ const HrReimbursment = () => {
   };
 
   const handleclick = (type) => {
-
     if (step === 0) {
       const ClinicalfilterData =
         ClinicalData &&
         ClinicalData.filter((data) => data.hospitalType === type);
       const Clinic = handleFilterData(ClinicalfilterData);
-      console.log("ClinicalfilterData",ClinicalfilterData, Clinic);
+      console.log("ClinicalfilterData", ClinicalfilterData, Clinic);
       setClinicTableData(Clinic);
-      setClinicalData(Clinic)
+      setClinicalData(Clinic);
     } else {
       const PharmacyfilterData =
         PharmacyData &&
@@ -212,7 +209,7 @@ const HrReimbursment = () => {
       const Pharmacy = handleFilterData(PharmacyfilterData);
       console.log("PharmacyfilterData", Pharmacy);
       setPharmacyTableData(Pharmacy);
-      setPharmacyData(Pharmacy)
+      setPharmacyData(Pharmacy);
     }
   };
 
@@ -249,14 +246,13 @@ const HrReimbursment = () => {
   const ReimbursmentCSV = ReimbursmentCSVData();
   // // CSV END
 
-
   return (
     <>
       {ReimbursmentPage && (
         <div>
           <div className="row d-flex align-items-center justify-content-between">
             <div className="col-lg-12 text-left">
-              <h3 className="mt-0 mb-4">Reimbursement</h3>
+              <h3 className="mt-0 mb-4 my-2">Reimbursement</h3>
             </div>
           </div>
 
@@ -298,75 +294,85 @@ const HrReimbursment = () => {
             </div>
             <div className="col-12 col-lg-6 col-md-6 text-right">
               <div className="search-btn">
-              <div className="col-12 col-lg-6 col-md-6 text-right">
-              <div className="search-btn">
-                <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Hospital"
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                  />
-                  <div className="input-group-append">
-                    <button
-                      className="btn btn-secondary"
-                      type="button"
-                      onClick={() => handleOnSearch()}
-                    >
-                      <i className="fa fa-search"></i>
-                    </button>
-                    </div>
+                <div className="search-btn">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control my-3"
+                      placeholder="Search Hospital"
+                      value={searchValue}
+                      onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                    <div className="input-group-append">
+                      <button
+                        className="btn btn-secondary my-3"
+                        type="button"
+                        onClick={() => handleOnSearch()}
+                      >
+                        <i className="fa fa-search"></i>
+                      </button>
                     </div>
                   </div>
-                </div>
-                <div class="btn-group hover_drop_down">
-                  <button
-                    type="button"
-                    class="btn btn-success btn-sm my-3"
-                    data-toggle="dropdown"
-                    style={{ width: "130px" }}
-                  >
-                    <i class="fas fa-filter"></i> Add Filters
-                  </button>
-                  <ul class="dropdown-menu" role="menu">
-                    <li>
-                      <a
-                        onClick={() => {
-                          handleclick("provincial");
-                        }}
+                  <div class="btn-group hover_drop_down">
+                    <button
+                      type="button"
+                      class="btn btn-success btn-md my-3 mx-2"
+                      data-toggle="dropdown"
+                      style={{
+                        width: "160px",
+                        borderRadius: "5px",
+                        backgroundColor: "#8EC131",
+                        border: "1px solid #8EC131",
+                      }}
+                    >
+                      <i class="fas fa-filter"></i> Add Filters
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                      <li>
+                        <a
+                          onClick={() => {
+                            handleclick("provincial");
+                          }}
+                        >
+                          provincial{" "}
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          onClick={() => {
+                            handleclick("public");
+                          }}
+                        >
+                          public
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          onClick={() => {
+                            handleclick("municipal");
+                          }}
+                        >
+                          Municipal
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-md my-3 mx-2"
+                    >
+                      <CSVLink
+                        data={ReimbursmentCSV}
+                        target="_blank"
+                        style={{ color: "white" }}
                       >
-                        provincial{" "}
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        onClick={() => {
-                          handleclick("public");
-                        }}
-                      >
-                        public
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        onClick={() => {
-                          handleclick("municipal");
-                        }}
-                      >
-                        Municipal
-                      </a>
-                    </li>
-                  </ul>
-                  <button type="button" class="btn btn-primary btn-sm my-3">
-                    <CSVLink data={ReimbursmentCSV} target="_blank">
-                      Download PDF/CSV
-                    </CSVLink>
-                  </button>
+                        Download PDF/CSV
+                      </CSVLink>
+                    </button>
+                  </div>
                 </div>
               </div>
-           
-              
             </div>
           </div>
           <div className="tab-content table-custome mt-3" id="myTabContent">
@@ -487,43 +493,43 @@ const HrReimbursment = () => {
                       </tbody>
                     ))}
                 </table>
-             
-              <div className="row">
-                <div className="col-md-6 col-sm-6 col-12">
-                  <small>Showing{PharmacyData.length}</small>
-                </div>
-                <div className="col-md-6 col-sm-6 col-12">
-                  <div className="pagination-custom">
-                    <nav aria-label="Page navigation example">
-                      <ul className="pagination justify-content-end">
-                        <li className="page-item disabled">
-                          <a className="page-link w-100">Previous</a>
-                        </li>
-                        <li className="page-item">
-                          <a className="page-link" href="#">
-                            1
-                          </a>
-                        </li>
-                        <li className="page-item">
-                          <a className="page-link active" href="#">
-                            2
-                          </a>
-                        </li>
-                        <li className="page-item">
-                          <a className="page-link" href="#">
-                            3
-                          </a>
-                        </li>
-                        <li className="page-item">
-                          <a className="page-link w-100" href="#">
-                            Next
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
+
+                <div className="row">
+                  <div className="col-md-6 col-sm-6 col-12">
+                    <small>Showing{PharmacyData.length}</small>
+                  </div>
+                  <div className="col-md-6 col-sm-6 col-12">
+                    <div className="pagination-custom">
+                      <nav aria-label="Page navigation example">
+                        <ul className="pagination justify-content-end">
+                          <li className="page-item disabled">
+                            <a className="page-link w-100">Previous</a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              1
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link active" href="#">
+                              2
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              3
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link w-100" href="#">
+                              Next
+                            </a>
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
                   </div>
                 </div>
-              </div>
               </div>
             </div>
           </div>
