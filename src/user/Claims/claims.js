b90e0b803f745821f6ce0ddc85800ae9294b848a
akashyadavnectar <akash.yadav@nectarinfotel.com>
2022-02-11 12:06:52 +0530

    claim page changed for user

--- src/user/Claims/claims.js
+++ src/user/Claims/claims.js
@@ -1,91 +1,108 @@
-import React, { useState,useEffect } from "react";
-import { Table, Input, Button,Breadcrumb,Dropdown,Modal, Menu,Form } from "antd";
-import {CSVLink} from "react-csv";
-import {DeleteOutlined,PlusOutlined,FilterOutlined,EditOutlined} from "@ant-design/icons"; 
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  Input,
+  Button,
+  Breadcrumb,
+  Dropdown,
+  Modal,
+  Menu,
+  Form,
+} from "antd";
+import { CSVLink } from "react-csv";
+import {
+  DeleteOutlined,
+  PlusOutlined,
+  FilterOutlined,
+  EditOutlined,
+} from "@ant-design/icons";
 import { useNavigate, useLocation } from "react-router-dom";
-import NewClaim from "./Newclaim"
-import UserClaim from './UserClaim'
+import NewClaim from "./Newclaim";
+import UserClaim from "./UserClaim";
 import moment from "moment";
-import {getClaimsList} from "../../services/authentication"
+import { getClaimsList } from "../../services/authentication";
 
-const Claims = () =>{
-  const [addClaim,setAddClaim] = useState(false)
-  const [claimTablePage, setclaimTablePage] = useState(true)
-  const[SelectedRecord,setSelectedRecord]=useState('')
-  const[ClaimDetailsPage,setClaimDetailsPage]=useState('')
-  const[ClaimsListArray,setClaimsListArray]=useState('')
-  const[TableData,setTableData]=useState('')
+const Claims = () => {
+  const [addClaim, setAddClaim] = useState(false);
+  const [claimTablePage, setclaimTablePage] = useState(true);
+  const [SelectedRecord, setSelectedRecord] = useState("");
+  const [ClaimDetailsPage, setClaimDetailsPage] = useState("");
+  const [ClaimsListArray, setClaimsListArray] = useState("");
+  const [TableData, setTableData] = useState("");
 
   let navigate = useNavigate();
 
- const handleClaimIdClick = (text,record)=>{
-  setclaimTablePage(false)
-  setSelectedRecord(record)
-  setClaimDetailsPage(true)
- }
+  const handleClaimIdClick = (text, record) => {
+    setclaimTablePage(false);
+    setSelectedRecord(record);
+    setClaimDetailsPage(true);
+  };
 
- const handleback =()=>{
-  setclaimTablePage(true)
-  setClaimDetailsPage(false)
- }
- const handleAddBack = () =>{
-  setAddClaim(false)
-  setclaimTablePage(true)
-}
- const handleGetServiceRequestCall = async () => {
-  try {
-    let tableDataArr = [];
-    const resp = await getClaimsList();
-    console.log("resp",resp)
-    setClaimsListArray(resp && resp.data);
-    resp &&
-      resp.data.map((data, i) => {
-        const value = {
-        id:data.claim_details ? data.claim_details.claim_id :'',
-        policyName:data.userPolicy.policy.policyName ? data.userPolicy.policy.policyName :'',
-        code:data.userPolicy.policy.policyCode ? data.userPolicy.policy.policyCode :'',
-        date:data.claim_details ? moment(data.claim_details.createdAt).format("MMM Do YY") : '',
-        status:data.verifyStatus ? data.verifyStatus :''
+  const handleback = () => {
+    setclaimTablePage(true);
+    setClaimDetailsPage(false);
+  };
+  const handleAddBack = () => {
+    setAddClaim(false);
+    setclaimTablePage(true);
+  };
+  const handleGetServiceRequestCall = async () => {
+    try {
+      let tableDataArr = [];
+      const resp = await getClaimsList();
+      console.log("resp", resp);
+      setClaimsListArray(resp && resp.data);
+      resp &&
+        resp.data.map((data, i) => {
+          const value = {
+            id: data.claim_details ? data.claim_details.claim_id : "",
+            policyName: data.userPolicy.policy.policyName
+              ? data.userPolicy.policy.policyName
+              : "",
+            code: data.userPolicy.policy.policyCode
+              ? data.userPolicy.policy.policyCode
+              : "",
+            date: data.claim_details
+              ? moment(data.claim_details.createdAt).format("MMM Do YY")
+              : "",
+            status: data.verifyStatus ? data.verifyStatus : "",
+          };
+          console.log("sasas", value);
+          tableDataArr.push(value);
+        });
+      console.log("tableDataArr in premium", tableDataArr);
+      setTableData(tableDataArr);
+      console.log("resp", resp);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  useEffect(() => {
+    handleGetServiceRequestCall();
+  }, []);
 
-        };
-        console.log("sasas",value);
-        tableDataArr.push(value);
-      });
-    console.log("tableDataArr in premium", tableDataArr);
-    setTableData(tableDataArr);
-    console.log("resp", resp);
-  } catch (error) {
-    console.log("error", error);
-  }
-};
-useEffect(() => {
-  handleGetServiceRequestCall();
-}, []);
-
-    
-const columns = [
+  const columns = [
     {
       title: "Claim Id",
       dataIndex: "id",
       key: "id",
       ellipsis: true,
-      render: (text,record) => (
+      render: (text, record) => (
         <a
           style={{ color: "#4cbb17" }}
-          onClick={() => handleClaimIdClick(text,record)}
+          onClick={() => handleClaimIdClick(text, record)}
         >
           {text}
         </a>
       ),
     },
 
-    
     {
-        title: "Policy Name",
-        dataIndex: "policyName",
-        key: "policyName",
-        ellipsis: true,
-      },
+      title: "Policy Name",
+      dataIndex: "policyName",
+      key: "policyName",
+      ellipsis: true,
+    },
 
     {
       title: "Policy Code",
@@ -99,7 +116,7 @@ const columns = [
       key: "date",
       ellipsis: true,
     },
-    
+
     {
       title: "Staus",
       dataIndex: "status",
@@ -108,77 +125,91 @@ const columns = [
     },
   ];
 
-  const handleAddClaims = () =>{
-    setAddClaim(true)
-    setclaimTablePage(false)
-  }
+  const handleAddClaims = () => {
+    setAddClaim(true);
+    setclaimTablePage(false);
+  };
 
-  return(
+  return (
     <div>
-    {claimTablePage &&
-   <div>
-    <Breadcrumb style={{ marginTop: "20px" }}>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
-      <Breadcrumb.Item>claims</Breadcrumb.Item>
-    </Breadcrumb>
-    <div
-        style={{
-          marginTop: "20px",
-          marginBottom: "25px",
-          display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "row",
-        }}
-      >
-        <div>
-          <h3>Claim Requests</h3>
-        </div>
-        <div style={{ display: "flex", flexDirection: "row" }}>
-
-          <Button
+      {claimTablePage && (
+        <div className="container-fluid">
+          <Breadcrumb style={{ marginTop: "20px" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>claims</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="row"
             style={{
-              borderRadius: "5px",
-              marginRight: "10px",
-              backgroundColor: "#61b33b",
-              color:"white"
+              marginTop: "20px",
+              marginBottom: "25px",
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "row",
             }}
-            onClick={()=>handleAddClaims()}
           >
-         <PlusOutlined style={{paddingTop:"5px"}}/> New Claim Request
-          </Button>
-         
-          <div>
-            <Button
-              style={{
-                color: "#ffffff",
-                backgroundColor: "#000089",
-              }}
+            <div className="col-12 col-sm-4 col-md-4">
+              <h3>Claim Requests</h3>
+            </div>
+            <div className="nav justify-content-left">
+            <div
+              className="col-12 col-sm-6 col-md-6"
+              style={{ display: "flex", flexDirection: "row" }}
             >
-              {/* Download PDF/CSV */}
-              <CSVLink data={""} target="_blank">
-                Download PDF/CSV
-              </CSVLink>
-            </Button>
+              <Button
+                style={{
+                  borderRadius: "5px",
+                  backgroundColor: "#61b33b",
+                  color: "white",
+                }}
+                onClick={() => handleAddClaims()}
+              >
+                <PlusOutlined style={{ paddingTop: "5px" }} /> New Claim Request
+              </Button>
+            </div>
+            <div className="col-12 col-sm-6 col-md-6">
+              <Button
+                style={{
+                  color: "#ffffff",
+                  backgroundColor: "#000089",
+                  borderRadius: "5px",
+                }}
+              >
+                {/* Download PDF/CSV */}
+                <CSVLink data={""} target="_blank">
+                  Download PDF/CSV
+                </CSVLink>
+              </Button>
+            </div>
+            </div>
+            
           </div>
+          <Table style={{padding:"10px"}}
+              columns={columns}
+              dataSource={TableData}
+              pagination={true}
+              total={10}
+            />
+            <div>
+              <span>shown Results {ClaimsListArray.length}</span>
+            </div>
         </div>
+      )}
+      {addClaim && (
+        <NewClaim
+          data={ClaimsListArray}
+          SelectedRecord={SelectedRecord}
+          handleBack={handleAddBack}
+        />
+      )}
+      {ClaimDetailsPage && (
+        <UserClaim
+          SelectedRecord={SelectedRecord}
+          data={ClaimsListArray}
+          handleback={handleback}
+        />
+      )}
     </div>
-      <Table
-        columns={columns}
-        dataSource={TableData}
-        pagination={true}
-        total={10}
-      />
-       <div><span>shown Results {ClaimsListArray.length}</span></div> 
-    </div>
-     }
-    {
-      addClaim && <NewClaim  data={ClaimsListArray} SelectedRecord={SelectedRecord} handleBack={handleAddBack}/>
-   
-    }
-    {
-         ClaimDetailsPage && <UserClaim SelectedRecord={SelectedRecord} data={ClaimsListArray}  handleback={handleback}/>
-    }
-    </div>
-  )
-} 
+  );
+};
 export default Claims;
